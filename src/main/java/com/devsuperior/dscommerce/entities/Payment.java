6e0582da5b35e22bf451c5cc1cf0396c5a6f7984
Diego Santana <3c.santana@gmail.com>
2024-04-04 20:26:15 -0300

    feat: criação do entidade Payment e o mapeamento e relacionamento OneToOne

---
+++ src/main/java/com/devsuperior/dscommerce/entities/Payment.java
@@ -0,0 +1,66 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    
+    @OneToOne
+    @MapsId // Define que o id de Payment vai ser o mesmo que o de Order, sendo a PK e FK 
+    private Order order;    
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Instant moment, Order order) {
+        this.id = id;
+        this.moment = moment;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Payment payment)) return false;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
