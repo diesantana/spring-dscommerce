85af1da7fc637c214f13b778e3e4059dc77c68b9
Diego Santana <3c.santana@gmail.com>
2024-04-08 09:39:04 -0300

    feat: Testando um  método GET com auxilio do repository

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,15 +1,25 @@
 package com.devsuperior.dscommerce.controllers;
 
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
     
+    @Autowired
+    private ProductRepository productRepository;
+    
     @GetMapping
     public String teste() {
-        return "Olá mundo!";
+        Optional<Product> optionalProduct = productRepository.findById(1L);
+        Product product = optionalProduct.get();
+        return product.getName();
     }
 }
