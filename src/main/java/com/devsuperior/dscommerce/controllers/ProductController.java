5f297f2208919c1e4a14274be14336206835c3ad
Diego Santana <3c.santana@gmail.com>
2024-04-08 20:24:00 -0300

    feat: Implementaçaõ do findById retornando um DTO

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.devsuperior.dscommerce.controllers;
 
+import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +17,10 @@ import java.util.Optional;
 public class ProductController {
     
     @Autowired
-    private ProductRepository productRepository;
+    private ProductService service;
     
-    @GetMapping
-    public String teste() {
-        Optional<Product> optionalProduct = productRepository.findById(1L);
-        Product product = optionalProduct.get();
-        return product.getName();
+    @GetMapping("/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+        return service.findById(id);
     }
 }
