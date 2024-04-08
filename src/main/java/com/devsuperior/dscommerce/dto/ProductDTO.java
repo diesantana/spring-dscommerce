1940cf43c46e0ed0ea1d297f900b7f16466914d4
Diego Santana <3c.santana@gmail.com>
2024-04-08 20:23:03 -0300

    feat: Criação do ProductDTO

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Product;
+import jakarta.persistence.Column;
+
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(String imgUrl, Double price, String description, String name, Long id) {
+        this.imgUrl = imgUrl;
+        this.price = price;
+        this.description = description;
+        this.name = name;
+        this.id = id;
+    }
+
+    public ProductDTO(Product entity) {
+        imgUrl = entity.getImgUrl();
+        price = entity.getPrice();
+        description = entity.getDescription();
+        name = entity.getName();
+        id = entity.getId();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
