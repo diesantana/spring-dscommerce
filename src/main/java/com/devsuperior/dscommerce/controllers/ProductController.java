6b6d0ad3fff77c15480c76abb1482497cdde3754
Diego Santana <3c.santana@gmail.com>
2024-05-06 12:15:50 -0300

    feat: Altera o retorno do findAll para ProductMinDTO

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.controllers;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
 import com.devsuperior.dscommerce.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +28,10 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(
+    public ResponseEntity<Page<ProductMinDTO>> findAll(
             @RequestParam(value = "name", defaultValue = "")  String name, 
             Pageable pageable) {
-        Page<ProductDTO> result = service.findAll(name, pageable);
+        Page<ProductMinDTO> result = service.findAll(name, pageable);
         return ResponseEntity.ok(result);
     }
 
