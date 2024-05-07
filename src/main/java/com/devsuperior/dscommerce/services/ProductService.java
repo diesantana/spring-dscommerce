b79aca71e2057e0a09f1e1101659e0c4451e6d57
Diego Santana <3c.santana@gmail.com>
2024-05-07 09:06:33 -0300

    feat: Implementa o carregamento dos Produtos com suas respectivas categorias

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,9 +1,12 @@
 package com.devsuperior.dscommerce.services;
 
 
+import com.devsuperior.dscommerce.dto.CategoryDTO;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.dto.ProductMinDTO;
+import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.CategoryRepository;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
@@ -18,20 +21,22 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
-
+    
     @Autowired
-    private ProductRepository repository;
+    private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-        Product product = repository.findById(id)
+        Product product = productRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
         return new ProductDTO(product);
     }
 
     @Transactional(readOnly = true)
     public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
-        Page<Product> result = repository.searchByName(name, pageable);
+        Page<Product> result = productRepository.searchByName(name, pageable);
         return result.map(ProductMinDTO::new);
     }
 
@@ -39,27 +44,31 @@ public class ProductService {
     public ProductDTO insert(ProductDTO productDTO) {
         Product entity = new Product();
         convertDtoToEntity(productDTO, entity);
-        return new ProductDTO(repository.save(entity));
+        return new ProductDTO(productRepository.save(entity));
     }
 
     @Transactional
     public ProductDTO update(Long id, ProductDTO productDTO) {
         try {
-            Product entity = repository.getReferenceById(id);
+            Product entity = productRepository.getReferenceById(id);
             convertDtoToEntity(productDTO, entity);
-            return new ProductDTO(repository.save(entity));
+            return new ProductDTO(productRepository.save(entity));
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Recurso não encontrado");
         }
     }
 
-    @Transactional(propagation = Propagation.SUPPORTS)
+    //  propagation = Propagation.SUPPORTS indica que o método deve ser executado em uma transação existente 
+    //  se já houver uma, mas não deve iniciar uma nova transação se não houver uma transação ativa.
+    // Sem essa anotação, o spring não consegue capturar a exceção relacionada ao banco de dados
+    // (DataIntegrityViolationException)
+    @Transactional(propagation = Propagation.SUPPORTS) 
     public void delete(Long id) {
-        if (!repository.existsById(id)) {
+        if (!productRepository.existsById(id)) {
             throw new ResourceNotFoundException("Recurso não encontrado");
         }
         try {
-            repository.deleteById(id);
+            productRepository.deleteById(id);
         }
         catch (DataIntegrityViolationException e) {
             throw new DatabaseException("Falha de integridade referencial");
@@ -72,5 +81,10 @@ public class ProductService {
         entity.setDescription(productDTO.getDescription());
         entity.setPrice(productDTO.getPrice());
         entity.setImgUrl(productDTO.getImgUrl());
+        entity.getCategories().clear();
+        for (CategoryDTO catDTO : productDTO.getCategories()) {
+            Category category = categoryRepository.getReferenceById(catDTO.getId());
+            entity.getCategories().add(category);
+        }
     }
 }
